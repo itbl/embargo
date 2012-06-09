09d55759ac84f65259010f58f940b297068accb3
milt <milt@jhu.edu>
2012-06-09 17:22:00 -0400

    added overrides for new and create to accolades and accusations controllers

--- app/controllers/accusations_controller.rb
+++ app/controllers/accusations_controller.rb
@@ -4,6 +4,22 @@ class AccusationsController < ReportsController
 		@reports = Accusation.page params[:page]
     end
 
+    def new
+      @report = Accusation.new
+    
+      respond_with @report
+    end
+
+    def create
+      @report = Accusation.new(params[:report])
+    
+      if @report.save
+        redirect_to @report, :notice => "#{@report.type} successfully created"
+      else
+        redirect_to new_accusation_path, :alert => "#{@report.type} could not be created"
+      end
+    end
+
     private
 
     def find_report

4e21a038daac476fa76c056ee341388890bbc951
Jason Lewis <jlewis73@jhu.edu>
2012-06-08 20:29:29 -0400

    Add Accusation controller logic

--- app/controllers/accusations_controller.rb
+++ app/controllers/accusations_controller.rb
@@ -1,14 +1,61 @@
 class AccusationsController < ApplicationController
-  before_filter :find_model
+  skip_before_filter :require_login, :only => :index
+
+  respond_to :html, :json
   
   def index
-    @accusations = 
+    @accusations = Accusation.page params[:page]
+    
+    respond_with @accusations
   end
-
   
-
-  private
-  def find_model
-    @model = Accusation.find(params[:id]) if params[:id]
+  def show
+    @accusation = Accusation.find(params[:id])
+    
+    respond_with @accusation
   end
+  
+  def new
+    @accusation = Accusation.new
+    
+    respond_with @accusation
+  end
+  
+  def edit
+    @accusation = Accusation.find(params[:id])
+    
+    respond_with @accusation
+  end
+  
+  def create
+    @accusation = Accusation.new(params[:accusation])
+    
+    if @accusation.save
+      redirect_to @accusation, :notice => "Accusation successfully created"
+    else
+      redirect_to new_accusation_path, :alert => "Accusation could not be created"
+    end
+  end
+  
+  def update
+    @accusation = Accusation.find(params[:id])
+    
+    if @accusation.update_attributes(params[:accusation])
+      redirect_to @accusation, :notice => "Accusation successfully updated"
+    else
+      redirect_to edit_accusation_path(@accusation), :alert => "Accusation could not be updated"
+    end
+  end
+
+  def delete
+    @accusation = Accusation.find(params[:id])
+    
+    if @accusation.destroy
+      redirect_to accusations_path, :notice => "Accusation deleted"
+    else
+      redirect_to @accusation, :alert => "An error has been"
+    end
+  end
+
+
 end

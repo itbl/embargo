a1d43ff6b3f12c9884a0dcffeddc287becdd9a4a
milt <milt@jhu.edu>
2012-06-09 12:01:37 -0400

    worked on tests

--- app/models/report.rb
+++ app/models/report.rb
@@ -2,6 +2,9 @@ class Report < ActiveRecord::Base
   belongs_to :client
   belongs_to :user
   attr_accessible :description, :title
+
+  validates :title, :presence => true, :length => { :in => 5..128 }
+  validates :description, :presence => true, :length => { :in => 100..2000 }
   
   paginates_per 25
   
--- spec/factories/accusations.rb
+++ spec/factories/accusations.rb
@@ -2,9 +2,9 @@
 
 FactoryGirl.define do
   factory :accusation do
-    title "MyString"
-    description "MyString"
-    accuser nil
-    accusable nil
+    title "They keep me to chained to a desk!"
+    description "And there are BEES in the office! Seriously, who has bees in the workplace? And do hey really have to cover us in honey?"
+    user nil
+    client nil
   end
 end

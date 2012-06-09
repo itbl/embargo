32f102b4daf438d011df10e643afea72d9e4ae28
milt <milt@jhu.edu>
2012-06-09 19:27:15 -0400

    unfucked controllers, removed default response and bangs on save

--- app/controllers/accolades_controller.rb
+++ app/controllers/accolades_controller.rb
@@ -1,16 +1,16 @@
 class AccoladesController < ApplicationController
   skip_before_filter :authenticate_user!, :only => :index
+  before_filter :find_accolade, :only => [ :show, :edit, :update, :delete ]
   
   respond_to :html, :json
   
   def index
     @accolades = Accolade.page params[:page]
     
-    respond_with @Accolades
+    respond_with @accolades
   end
   
   def show
-    @accolade = Accolade.find(params[:id])
     respond_with @accolade
   end
   
@@ -21,15 +21,13 @@ class AccoladesController < ApplicationController
   end
   
   def edit
-    @accolade = Accolade.find(params[:id])
-
     respond_with @accolade
   end
   
   def create
     @accolade = Accolade.new(params[:accolade])
     
-    if @accolade.save!
+    if @accolade.save
       redirect_to @accolade, :notice => "Accolade successfully created"
     else
       redirect_to new_accolade_path, :alert => "Accolade could not be created"
@@ -37,7 +35,6 @@ class AccoladesController < ApplicationController
   end
   
   def update
-    @accolade = Accolade.find(params[:id])
     if @accolade.update_attributes(params[:accolade])
       redirect_to @accolade, :notice => "Accolade successfully updated"
     else
@@ -46,11 +43,17 @@ class AccoladesController < ApplicationController
   end
 
   def delete
-    @accolade = Accolade.find(params[:id])
     if @accolade.destroy
       redirect_to accolades_path, :notice => "Accolade deleted"
     else
       redirect_to @accolade, :alert => "An error has been"
     end
   end
+
+  private
+
+  def find_accolade
+    @accolade = Accolade.find(params[:id])
+  end
+
 end
